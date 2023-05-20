f7c83b9ea5084c1a0b13576536090a52111415e4
kr4k3nh4ck <umuttechin@gmail.com>
2023-05-20 13:07:57 +0000

    env file for password fixed

--- lsfunctest.sql
+++ lsfunctest.sql
@@ -1,9 +1,1 @@
-connect_opts=
-
-[ -z $host ] || connect_opts+="-h $host "
-[ -z $port] || connect_opts+="-P $port "
-[ -z $pass_wd] || connect_opts+="-p $pass_wd "
-
-echo $connect_opts
-psql -d $database -U $user $connect_opts -c "select pronamespace::regnamespace, proname, pg_get_functiondef(oid) from pg_proc where pronamespace  in (select oid from pg_namespace where nspname not in ('pg_toast', 'pg_catalog', 'information_schema')) and proname != 'group_concat';"
-
+psql -c "select pronamespace::regnamespace as schema, proname as function_name, pg_get_functiondef(pr.oid) as function_def from pg_proc pr join pg_namespace pn on pr.pronamespace = pn.oid where pn.nspname not in ('pg_toast', 'pg_catalog', 'information_schema') and prokind != 'a';"
--- setenv.sh
+++ setenv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-unset o OPTARG OPTIND database user
+unset o OPTARG OPTIND database user host port pass_wd
 
-usage() { echo "Usage: source setenv -d <db_name> -u <username> [-p <password>] [-h <host>] [-P <port>]" 1>&2; }
+usage() { echo "Usage: source setenv -d <db_name> -u <username> [-w <password>] [-h <host>] [-p <port>]" 1>&2; }
 
 while getopts ":d:u:h:P:p" o; do
     case "${o}" in
@@ -15,10 +15,10 @@ while getopts ":d:u:h:P:p" o; do
         h)
             host=${OPTARG}
             ;;	
-        P)
+        p)
             port=${OPTARG}
             ;;
-        p)
+        w)
             read -s -r -p "Password: " pass_wd
             ;;			
         *)
@@ -32,18 +32,24 @@ if [ -z "${database}" ] || [ -z "${user}" ]; then
     usage
 fi
 
+echo "[uts]" > ./.pg_service.conf
 
+#export database=$database
+#export user=$user
+
+echo "dbname=$database"  >> ./.pg_service.conf 
+echo "user=$user"  >> ./.pg_service.conf 
+
+[ -z "$host" ] && echo "host=localhost"  >> ./.pg_service.conf || echo "host=$host"  >> ./.pg_service.conf 
+[ -z "$port" ] && echo "port=5432"  >> ./.pg_service.conf || echo "port=$port"  >> ./.pg_service.conf
+[ -z "$pass_wd" ] || echo "password=$pass_wd" >> ./.pg_service.conf
+
+
+export PGSERVICE=uts
+export PGSERVICEFILE=./.pg_service.conf
 export PAGER=less
 export LESS='-iMFXSx4R'
 
-
-
-
-export database=$database
-export user=$user
-[ -z "$host" ] || export host=$host
-[ -z "$port" ] || export port=$port
-[ -z "$pass_wd" ] || export pass_wd=$pass_wd
 
 echo -e "\n"
 
