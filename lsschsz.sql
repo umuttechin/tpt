fe013fc2f17e0dac0735fb3dce5365d89d25eb31
kr4k3nh4ck <umuttechin@gmail.com>
2023-05-22 21:37:47 +0000

    schema size sql modified.

--- lsschsz.sql
+++ lsschsz.sql
@@ -1,11 +1,52 @@
-psql -d $database -U $user -c "SELECT schema_name,
-       SUM(table_size) :: bigint / 1024 AS schema_size_in_kb,
-       round(( SUM(table_size) / Pg_database_size(Current_database()) ) * 100, 2) AS
-       percentage
-FROM   (SELECT pg_catalog.pg_namespace.nspname           AS schema_name,
-               Pg_relation_size(pg_catalog.pg_class.oid) AS table_size
-        FROM   pg_catalog.pg_class
-               join pg_catalog.pg_namespace
-                 ON relnamespace = pg_catalog.pg_namespace.oid) t
-GROUP  BY rollup(schema_name)
-ORDER  BY 2 DESC; "
+#!/bin/bash
+
+unset o OPTARG OPTIND
+
+usage() { echo "Usage Examples:" 1>&2;
+          echo "./lsschsz.sql" 1>&2;
+          echo "./lsschsz.sql \"schema name = '\$schema_name'\"" 1>&2;
+          echo "";
+ exit 1; }
+
+while getopts ":h" o; do
+    case "${o}" in
+        h)
+            usage
+                        ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+condition=$1
+
+if [ -z "${condition}" ]; then
+	psql -c "select schema_name,
+       round(sum(table_size) / 1024, 2)                      as size_in_mb,
+       round(sum(table_size) * 100 / database_size, 2)      as percentage
+from   (select pg_catalog.pg_namespace.nspname     as schema_name,
+        pg_relation_size(pg_catalog.pg_class.oid) as table_size,
+        sum(pg_relation_size(pg_catalog.pg_class.oid))
+        over ()                                    as database_size
+        from   pg_catalog.pg_class
+        join pg_catalog.pg_namespace
+        on relnamespace = pg_catalog.pg_namespace.oid) t   
+group  by schema_name, database_size
+order  by 2 desc; "
+else
+    psql -c "select schema_name,
+       round(sum(table_size) / 1024, 2)                      as size_in_mb,
+       round(sum(table_size) * 100 / database_size, 2)      as percentage
+from   (select pg_catalog.pg_namespace.nspname     as schema_name,
+        pg_relation_size(pg_catalog.pg_class.oid) as table_size,
+        sum(pg_relation_size(pg_catalog.pg_class.oid))
+        over ()                                    as database_size
+        from   pg_catalog.pg_class
+        join pg_catalog.pg_namespace
+        on relnamespace = pg_catalog.pg_namespace.oid) t
+	    where $condition     
+group  by schema_name, database_size
+order  by 2 desc; " 2>/dev/null || usage
+fi
