3199039580dd75110e2589e7c62a1165c248d7a4
UmutTEKİN <umuttechin@gmail.com>
2023-06-05 23:47:41 +0200

    Update dwe_ash.sql

--- dwe_ash.sql
+++ dwe_ash.sql
@@ -3,8 +3,8 @@
 unset o OPTARG OPTIND
 
 usage() { echo "Usage Examples:" 1>&2;
-          echo "./aas_ash.sql" 1>&2;
-          echo "./ass_ash.sql \"'\$time_interval'\"" 1>&2;
+          echo "./dwe_ash.sql" 1>&2;
+          echo "./dwe_ash.sql \"'\$time_interval'\"" 1>&2;
           echo "";
  exit 1; }
 
